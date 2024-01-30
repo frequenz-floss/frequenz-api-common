23a4bb7a98dcd5590bb14a811aa1282cc060db71
Leandro Lucarella <luca-frequenz@llucax.com>
2024-01-30 11:46:31 +0100

    Add backwards compatibility with v0.3.x
    This release includes the top-level files present in the v0.3.x release,
    so the Microgrid API v0.15.x can depend on a newer version of the Common
    API.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -3,3 +3,6 @@
 ## New Features
 
 - Add a energy message for electricity trading markets
+- Add backwards compatibility with v0.3.x
+
+  This release includes the top-level files present in the v0.3.x release, so the Microgrid API v0.15.x can depend on a newer version of the Common API.
---
+++ pytests/test_v0.py
@@ -0,0 +1,51 @@
+# License: MIT
+# Copyright © 2023 Frequenz Energy-as-a-Service GmbH
+
+"""Tests for the v0.3.x frequenz.api.common package."""
+
+
+def test_package_import() -> None:
+    """Test that the package can be imported."""
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api import common
+
+    assert common is not None
+
+
+def test_module_import_components() -> None:
+    """Test that the modules can be imported."""
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api.common import components_pb2
+
+    assert components_pb2 is not None
+
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api.common import components_pb2_grpc
+
+    assert components_pb2_grpc is not None
+
+
+def test_module_import_metrics() -> None:
+    """Test that the modules can be imported."""
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api.common import metrics_pb2
+
+    assert metrics_pb2 is not None
+
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api.common import metrics_pb2_grpc
+
+    assert metrics_pb2_grpc is not None
+
+
+def test_module_import_metrics_electrical() -> None:
+    """Test that the modules can be imported."""
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api.common.metrics import electrical_pb2
+
+    assert electrical_pb2 is not None
+
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api.common.metrics import electrical_pb2_grpc
+
+    assert electrical_pb2_grpc is not None
