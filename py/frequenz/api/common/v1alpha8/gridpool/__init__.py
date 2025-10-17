07c70e4589507d40ceda88bc459e725439bc65ee
Richard Sandoval <rsandovaldev@gmail.com>
2025-10-17 13:56:24 +0200

    feat(gridpool): add gridpool definition to v1alpha8
    Introduce Gridpool message with id and name fields. A gridpool represents
    an assigned group of microgrids for the purpose of energy trading.
    
    Signed-off-by: Richard Sandoval <rsandovaldev@gmail.com>

--- py/frequenz/api/common/v1alpha8/gridpool/__init__.py
+++ py/frequenz/api/common/v1alpha8/gridpool/__init__.py
@@ -1,4 +1,4 @@
 # License: MIT
 # Copyright © 2025 Frequenz Energy-as-a-Service GmbH
 
-"""Frequenz common gRPC API and bindings for v1alpha8."""
+"""Gridpool bindings for Frequenz common gRPC API."""
